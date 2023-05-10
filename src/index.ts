404e22ee7b2635f56e5e5161fe90aab24d4eb516
Kong Yang <kpyang2414@gmail.com>
2023-05-09 21:24:27 -0500

    start of ts fundamentals through ts handbook doc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+
+// Below code used to learn how to compile TS into JS in the dist folder. 
 console.log('Hello World');
 
 let age: number = 25
@@ -5,4 +7,8 @@ let age: number = 25
     age += 10;
  }
 console.log(age);
-    
+
+
+// The rest of typesript fundamental will be referencing tyepscript handbook
+// Link: https://www.typescriptlang.org/docs/handbook/intro.html
+
