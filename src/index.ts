1cb29bb3b291a4bd2445d773ee86f87b36d59b6c
Kong Yang <kpyang2414@gmail.com>
2023-05-09 21:55:09 -0500

    end of day 5-9-23

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 // Below code used to learn how to compile TS into JS in the dist folder. 
+// Pulled from this tutorial: https://www.youtube.com/watch?v=d56mG7DezGs&t=506s
 console.log('Hello World');
 
 let age: number = 25
@@ -11,4 +12,7 @@ console.log(age);
 
 // The rest of typesript fundamental will be referencing tyepscript handbook
 // Link: https://www.typescriptlang.org/docs/handbook/intro.html
+
+// Started on 9-5-23
+// The Basics
 
