719ac2b3b5e8e7dbc608eb56c10fcadc58f335fd
Kong Yang <kpyang2414@gmail.com>
2023-05-18 21:32:58 -0500

    added some examples from handbook>everdaytypes

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,34 @@
 "use strict";
 console.log('Hello World');
 let age = 25;
-if (age < 50)
+if (age < 50) {
     age += 10;
+}
 console.log(age);
+function createUser(firstName, date) {
+    console.log(`${firstName}, today is ${date}`);
+}
+createUser("Greg", new Date());
+let thisString;
+let thisNumber;
+let thisBoolean;
+let numberArray;
+let stringArray;
+Array;
+let thisObj = { x: 0 };
+let thisVariable = true;
+function sayHello(greetingMessage, person) {
+    console.log(`${greetingMessage}, ${person}!`);
+}
+sayHello("Hello there", "David");
+function addNumbers(x, y) {
+    let answer = x + y;
+    return answer.toString();
+}
+console.log(typeof (addNumbers(4, 5)));
+console.log(addNumbers(4, 5));
+const names = ["alice", "bob", 'claire'];
+names.forEach(function (s) {
+    console.log(s.toUpperCase());
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,109 @@ console.log(age);
 // The rest of typesript fundamental will be referencing tyepscript handbook
 // Link: https://www.typescriptlang.org/docs/handbook/intro.html
 
-// Started on 9-5-23
+// Started on 5-9-23
 // The Basics
 
+// Creating a function with defined types for each parameter
+function createUser(firstName: string, date: Date) {
+    console.log(`${firstName}, today is ${date}`);    
+}
+
+// incorrect: error: Argument of type 'string' is not assignable to parameter of type 'Date'.
+// createUser("Greg", Date());
+// correct
+createUser("Greg", new Date());
+
+
+// EVERYDAY TYPES
+// Primitives are string, number and boolean
+// Always use undercase for these types in Typescript
+let thisString: string;
+let thisNumber: number;
+let thisBoolean: boolean;
+
+// ARRAY
+// To specify a an array type like [1,2,3] or ["a", "b", "c"] use <Type>[].
+// For example:
+
+let numberArray: number[];
+// or
+let stringArray: string[];
+
+// This is a generic form, look at Generics
+Array<number>
+// NOTE: [number] is completely different from Array[], this is in tuple section
+
+
+// any TYPE
+// "any" type can be use whenever I don’t want a particular value to cause typechecking errors.
+let thisObj: any = {x: 0}
+
+// None of the following lines of code will throw compiler errors but it will throw errors in debug
+// Using `any` disables all further type checking, and it is assumed 
+// you know the environment better than TypeScript.
+// thisObj.foo();
+// thisObj();
+// thisObj.bar = 100;
+// thisObj = "hello";
+// const n: number = thisObj;
+
+
+// noImplicitAny
+// When you don’t specify a type, and TypeScript can’t infer it from context, the compiler will typically default to any.
+// You usually want to avoid this, though, because any isn’t type-checked. Use the compiler flag noImplicitAny to flag any implicit any as an error.
+
+
+// Type annotation on variables
+let thisVariable: boolean = true;
+
+
+
+// FUNCTIONS
+// This is the primary way of passing data around in Javascript. 
+// Typescript allows us to specify both the INPUT and OUTPUT VALUES of the function.
+
+// parameter type annotation
+function sayHello(greetingMessage: string, person: string) {
+    console.log(`${greetingMessage}, ${person}!`);
+}
+
+// correct
+sayHello("Hello there", "David")
+
+// Type error will be thrown
+// Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+// sayHello(23, "david")
+
+
+// Having return types.
+// In this example, I had 2 parameters, which are both number types BUT it returns a string.
+// Transform the answer variable of type number to string
+function addNumbers(x: number, y: number): string {
+    let answer: number = x + y
+    return answer.toString()
+}
+
+console.log(typeof(addNumbers(4,5)));
+console.log(addNumbers(4,5));
+
+
+// Anonymous functions are a little bit different from function declarations.
+// When a function appears in a place where TypeScript can determine how it’s
+// going to be called, the parameters of that function are automatically given types.
+const names = ["alice", "bob", 'claire']
+
+// anonymous function
+names.forEach(function(eachName) {
+    // auto-assign the type string to eachName
+    console.log(eachName.toUpperCase());
+})
+// arror function
+names.forEach((eachName) => {
+     // auto-assign the type string to eachName
+    console.log(eachName.toUpperCase());
+})
+// This is called contextual typing
+
+
+// OBJECT TYPES
